71b6701553068ecc84b805fb89a6b0f08b331fbd
kaivalyaanugula9330 <n01659330@humber.ca>
2026-02-19 11:13:52 -0500

    Renamed MainActivity to KaivalyaActivity5 as per lab requirement

--- KaivalyaLab5/src/main/java/kaivalya/anugula/fragmentlab5/KaivalyaActivity5.java
+++ KaivalyaLab5/src/main/java/kaivalya/anugula/fragmentlab5/KaivalyaActivity5.java
@@ -2,13 +2,9 @@ package kaivalya.anugula.fragmentlab5;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class KaivalyaActivity5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
